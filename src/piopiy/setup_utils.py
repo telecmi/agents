66f8f5e1a41bdec1b4b5b20d6307bd3ba7c02344
SachinTelecmi <sachin@telecmi.com>
2025-09-18 10:03:47 +0000

    silero download link updated

--- src/piopiy/setup_utils.py
+++ src/piopiy/setup_utils.py
@@ -95,7 +95,7 @@ def download_silero_model():
         return
     
     # Model URL (English TTS model)
-    model_url = "https://github.com/snakers4/silero-vad/blob/master/src/silero_vad/data/silero_vad.onnx"
+    model_url = "https://huggingface.co/onnx-community/silero-vad/resolve/main/onnx/model.onnx"
     
     def download_with_progress(url, filepath):
         """Download file with progress indication."""
